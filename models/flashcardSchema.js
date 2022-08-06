231512c701a521376a3c24de75d906fb69d22f90
George Zeng <georgexzeng@outlook.com>
2022-08-06 19:45:16 +0100

    Connect mongoose and initial flashcard CRUD API created

---
+++ models/flashcardSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const flashcardSchema = new mongoose.Schema(
+    {
+        front: String,
+        back: String
+    });
+
+const Flashcard = mongoose.model('Flashcard', flashcardSchema);
+
+module.exports=Flashcard
--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ if(process.env.NODE_ENV !== "production"){
 }
 
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
+
+// Mongoose models
+const Flashcard = require('../models/flashcardSchema');
 
 /**
  * App variables
@@ -17,10 +21,25 @@ const path = require('path')
 const PORT = process.env.PORT || 5000;
 
 /**
- * App Configuration
+ * Mongoose connection
  */
 
+ mongoose.connect("mongodb://localhost:27017/socialStudy")
+ .then( ()=>{
+     console.log("MongoDB connected")
+ })
+ .catch( err => {
+     console.log(`MonogDB Connection error - ${err}`)
+ })
+
+/**
+ * App Configuration
+ */
 const app = express()
+
+// JSON body parser & url encoded bodies
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Routes Definitions
@@ -29,6 +48,54 @@ const app = express()
 app.get("/api", (req, res) => {
     res.send({message: "Server Responding!!"})
 })
+
+// Flashcard routes
+app.get("/api/flashcards", async(req, res, next)=> {
+    try {
+        const flashcards = await Flashcard.find(); 
+        res.send(flashcards)
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.post("/api/flashcards/new", async (req, res, next) => {
+    try {
+        const flashcard = new Flashcard(req.body); 
+        await flashcard.save()
+        res.send({message: "flashcard created"})
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.patch("/api/flashcards/:f_id", async (req, res, next) => {
+    try {
+        const updatedFlashcard = await Flashcard.findByIdAndUpdate(
+            req.params.f_id,
+            req.body,
+            { runValidators: true }
+        )
+        await updatedFlashcard.save()
+        res.send({
+            old: updatedFlashcard,
+            message: "flashcard updated"
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.delete("/api/flashcards/:f_id", async (req, res, next) => {
+    try {
+        const deletedFlashcard = await Flashcard.findByIdAndDelete(req.params.f_id)
+        res.send({old: deletedFlashcard, message: "flashcard deleted"})
+    } catch (error) {
+        next(error)
+    }
+})
+
+
 
 /**
  * Server Activation
