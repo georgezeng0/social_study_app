f87aa23f203961990e9839091e8037730bb2eb4e
George Zeng <georgexzeng@outlook.com>
2022-12-05 16:47:16 +0000

    Change env variables for prod

--- app/src/utils/getToken.js
+++ app/src/utils/getToken.js
@@ -1,6 +1,6 @@
 export default async function (getAccessTokenSilently) {
     return getAccessTokenSilently({
-        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE || "http://localhost:3000/",
         scope: "user",
       });
 }
